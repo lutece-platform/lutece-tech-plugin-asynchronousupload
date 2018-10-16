7dc9e154335cfc594b69e152d9a9fba47805c539
thomas dumont <thomas.dumont@paris.fr>
2018-10-16 20:31:25 +0200

    ASYNCUP-29 : Display the uploaded file size

--- webapp/WEB-INF/templates/skin/plugins/asynchronousupload/main.js
+++ webapp/WEB-INF/templates/skin/plugins/asynchronousupload/main.js
@@ -153,12 +153,32 @@ function formDisplayUploadedFiles${fieldname}( jsonData, files, cbPrefix )
 					if (typeof(imgContent) == "string" && imgContent.length > 0) {
 						imgTag = " <img src="+"'"+ imgContent +"'"+"alt='' "+" width='${previewMaxWidth}' height='${previewMaxHeight}'/>";
 					}
+
+                    var sizeDisplay = "";
+                    var octetUnit;
+                    var octetNumber;
+                    if (jsonData.files[index].size) {
+                        if (jsonData.files[index].size < 1024) {
+                            octetUnit = "o";
+                            octetNumber = file.size;
+                        }
+                        else if (jsonData.files[index].size < 1024 * 1024) {
+                            octetUnit = "Ko";
+                            octetNumber = jsonData.files[index].size/1024;
+                        }
+                        else {
+                            octetUnit = "Mo";
+                            octetNumber = jsonData.files[index].size/(1024*1024);
+                        }
+                        sizeDisplay = " (" + Math.floor(octetNumber) + " " + octetUnit + ")";
+                    }
+
 					strContent = strContent + "<div class=\"checkbox\" id=\"_file_uploaded_" + fieldName + index + "\"><label class=\"col-xs-12 col-sm-12 col-md-12 col-lg-12\">  \
 								<input type=\"checkbox\"  \
 									name=\"" + checkboxPrefix + index + "\"  \
 									id=\"" + checkboxPrefix + index + "\"  \
 								/>  \
-								&#160;" + ( (jsonData.fileCount == 1) ? jsonData.files.name : jsonData.files[index].name ) + 
+								&#160;" + ( (jsonData.fileCount == 1) ? jsonData.files.name : jsonData.files[index].name ) + sizeDisplay +
 							"</label></div><div class=\"col-xs-12 col-sm-12 col-md-12 col-lg-12\">"+imgTag+"</div>";
 //				}
 //				else if ( jsonData.files[index].is_removed )
