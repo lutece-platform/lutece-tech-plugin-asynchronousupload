d93c7752eb4b31e0e5f2a893e72fc1cbe845dfcd
HOHL <laurent.hohl@gmail.com>
2021-09-15 09:37:31 +0200

    ASYNCUP-53 : Improve admin skin

--- webapp/js/admin/plugins/asynchronousupload/config.js
+++ webapp/js/admin/plugins/asynchronousupload/config.js
@@ -17,19 +17,16 @@ function getAdminTemplateUploadedFile( fieldName, index, checkboxPrefix, jsonDat
 			octetNumber = sizeTemp/(1024*1024);
 		}
 		sizeDisplay = "" + Math.floor(octetNumber) + " " + octetUnit;
-   }
-
-   if( fileDisplayName.length > 24 ){
+   	}
+   	if( fileDisplayName.length > 24 ){
 	   fileDisplayName = fileName.substr(0,20) + '...';
-   }
-
+   	}
 	if ( typeof (imgContent) == "string" && imgContent.length > 0) {
 		sImg = '<img src=' + '"' + imgContent + '"' + 'alt="" style="width:40px;" />';
 	} else {
-		sImg='<div aria-hidden="true" class="icon-type-item" data-type="' + mimeTypeDisplay + '"></div>';
+		sImg = '<svg class="bd-placeholder-img" width="40" height="40" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: ' + mimeTypeDisplay + '" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#9e9e9e"></rect><text x="15%" y="50%" fill="#fff" dy=".3em" style="text-transform:uppercase;">' + mimeTypeDisplay + '</text></svg>';
 	}
-
 	var strName = ( (jsonData.fileCount == 1) ? jsonData.files.name : jsonData.files[index].name );
-   strCode ="<div class=\"list-group-item list-file-item\" id=\"_file_uploaded_" + fieldName + index + "\"><label for=\"" + checkboxPrefix + index + "\"><input type=\"checkbox\" class=\"uploaded_check\" name=\"" + checkboxPrefix + index + "\" id=\"" + checkboxPrefix + index + "\" ><figure>" + sImg + "<figcaption><span class=\"truncate\" title=\"" + strName + "\">" + strName + "</span><small>[ " + sizeDisplay + " ]</small></figcaption></figure></label><a href=\"#\" class=\"btn btn-danger deleteSingleFile\" fieldName=\"" + fieldName + "\" handlerName=\"" + handler + "\" index=\"" + index + "\"><i class=\"fa fa-trash fa-fw\"></i></a></div>";
-   return strCode;
+   	strCode ="<div class=\"list-file-item\"><figure>" + sImg + "</figure><div><label for=\"" + checkboxPrefix + index + "\"><input type=\"checkbox\" class=\"uploaded_check\" name=\"" + checkboxPrefix + index + "\" id=\"" + checkboxPrefix + index + "\" ><span class=\"truncate\" title=\"" + strName + "\">" + strName + "</span><small>[ " + sizeDisplay + " ]</small></label></div><div><a href=\"#\" class=\"btn btn-danger button is-danger is-small deleteSingleFile\" fieldName=\"" + fieldName + "\" handlerName=\"" + handler + "\" index=\"" + index + "\"><i class=\"fa fa-trash fa-fw\"></i></a></div></div>";
+   	return strCode;
 }
