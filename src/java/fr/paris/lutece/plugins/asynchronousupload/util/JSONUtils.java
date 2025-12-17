090a4e87a4c822fc895dc0b1ae82c3a70adf0f0d
evdngsl <158455076+evdngsl@users.noreply.github.com>
2025-12-17 21:30:01 +0100

    LUT-31259 : Migrate net.sf.json-lib to Jackson (Uploaded files list fails when list contains only one file)
    (cherry picked from commit 1357474e48b9649fe3fa25d96bc79cd3207beece)

--- src/java/fr/paris/lutece/plugins/asynchronousupload/util/JSONUtils.java
+++ src/java/fr/paris/lutece/plugins/asynchronousupload/util/JSONUtils.java
@@ -94,25 +94,40 @@ public final class JSONUtils
         ObjectMapper mapper = new ObjectMapper( );
         ObjectNode json = mapper.createObjectNode( );
 
-        if ( listFileItem != null )
+        if ( listFileItem != null && !listFileItem.isEmpty( ) )
         {
-            ArrayNode uploadedFilesArray = mapper.createArrayNode();
-            
-            for ( MultipartItem fileItem : listFileItem )
+            if ( 1 == listFileItem.size( ) )
             {
                 ObjectNode jsonObject = mapper.createObjectNode( );
+                MultipartItem fileItem = listFileItem.get( 0 );
                 jsonObject.put( JSON_KEY_FILE_NAME, fileItem.getName( ) );
                 jsonObject.put( JSON_KEY_FILE_PREVIEW, getPreviewImage( fileItem ) );
                 jsonObject.put( JSON_KEY_FILE_SIZE, fileItem.getSize( ) );
-                uploadedFilesArray.add( jsonObject );
+                
+                json.set( JSON_KEY_UPLOADED_FILES, jsonObject );
+                json.put( JSON_KEY_FILE_COUNT, 1 );
             }
+            else
+            {
+                ArrayNode uploadedFilesArray = mapper.createArrayNode( );
 
-            json.set(JSON_KEY_UPLOADED_FILES, uploadedFilesArray);
-            json.put(JSON_KEY_FILE_COUNT, listFileItem.size());
+                for ( MultipartItem fileItem : listFileItem )
+                {
+                    ObjectNode jsonObject = mapper.createObjectNode( );
+                    jsonObject.put( JSON_KEY_FILE_NAME, fileItem.getName( ) );
+                    jsonObject.put( JSON_KEY_FILE_PREVIEW, getPreviewImage( fileItem ) );
+                    jsonObject.put( JSON_KEY_FILE_SIZE, fileItem.getSize( ) );
+                    uploadedFilesArray.add( jsonObject );
+                }
+
+                json.set( JSON_KEY_UPLOADED_FILES, uploadedFilesArray );
+                json.put( JSON_KEY_FILE_COUNT, listFileItem.size( ) );
+            }
         }
         else
         {
-            // no file
+            
+        	// no file
             json.put( JSON_KEY_FILE_COUNT, 0 );
         }
 
